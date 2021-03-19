4b9ca8d0d29ca34bff53bf24ce2767228e2f9431
Pedro Fonseca <peidrao>
2021-03-19 20:01:02 -0300

    add: controller de usuário (adição de usuário)

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import UserRepository from '../repositories/UserRepository'
+import CreateUserService from '../services/User/CreateUserService'
+
+export default class UserController {
+  public async create (request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body
+
+    const userRepository = new UserRepository()
+    const createUserService = new CreateUserService(userRepository)
+
+    const user = await createUserService.execute({ name, email, password })
+
+    // delete user.password
+
+    return response.json(user)
+  }
+}
