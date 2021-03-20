854c4946796de36c7ac012d058e12fd685c6a78d
Pedro Fonseca <peidrao>
2021-03-20 10:33:10 -0300

    rota e controles de sessão criadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import userRoutes from './user'
+import sessionRoutes from './session'
 
 const prefix = '/api/v1'
 const routes = Router()
 
 routes.use(`${prefix}/users`, userRoutes)
+routes.use(`${prefix}/session`, sessionRoutes)
 
 export default routes
