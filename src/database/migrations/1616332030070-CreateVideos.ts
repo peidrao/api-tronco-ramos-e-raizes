4eecb369c1863d61aed6c7634b209d9a19bf1925
Pedro Fonseca <peidrao>
2021-03-22 18:54:25 -0300

    add: migration para criar vídeos

--- src/database/migrations/1616332030070-CreateVideos.ts
+++ src/database/migrations/1616332030070-CreateVideos.ts
@@ -1,50 +1,54 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey
+} from 'typeorm'
 
 export default class CreateVideos1616332030070 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: 'videos',
-        columns: [
-          {
-            name: 'id',
-            type: 'varchar',
-            generationStrategy: 'uuid',
-            isPrimary: true
-          },
-          {
-            name: 'user_id',
-            type: 'varchar',
-            generationStrategy: 'uuid'
-          },
+  private table = new Table({
+    name: 'videos',
+    columns: [
+      {
+        name: 'id',
+        type: 'varchar',
+        generationStrategy: 'uuid',
+        isPrimary: true
+      },
+      {
+        name: 'user_id',
+        type: 'varchar',
+        generationStrategy: 'uuid'
+      },
 
-          {
-            name: 'title',
-            type: 'varchar'
-          },
-          {
-            name: 'link',
-            type: 'varchar'
-          },
-          {
-            name: 'description',
-            type: 'text'
-          },
-          { name: 'created_at', type: 'timestamp', default: 'now()' },
-          { name: 'updated_at', type: 'timestamp', default: 'now()' }
-        ],
-        foreignKeys: [
-          {
-            name: 'VideosByUsers',
-            columnNames: ['user_id'],
-            referencedColumnNames: ['id'],
-            referencedTableName: 'users',
-            onDelete: 'SET NULL',
-            onUpdate: 'CASCADE'
-          }
-        ]
-      })
-    )
+      {
+        name: 'title',
+        type: 'varchar'
+      },
+      {
+        name: 'link',
+        type: 'varchar'
+      },
+      {
+        name: 'description',
+        type: 'text'
+      },
+      { name: 'created_at', type: 'timestamp', default: 'now()' },
+      { name: 'updated_at', type: 'timestamp', default: 'now()' }
+    ]
+  });
+
+  private foreignKey = new TableForeignKey({
+    name: 'VideosByUsers',
+    columnNames: ['user_id'],
+    referencedColumnNames: ['id'],
+    referencedTableName: 'users',
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE'
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
