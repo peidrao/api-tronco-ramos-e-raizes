e7d281b42d7f92fc16903d2a29ab97b5aa62b99a
Pedro Fonseca <peidrao>
2021-03-22 21:48:55 -0300

    edit: migration de video funcionando

--- src/database/migrations/1616332030070-CreateVideos.ts
+++ src/database/migrations/1616332030070-CreateVideos.ts
@@ -2,6 +2,7 @@ import {
   MigrationInterface,
   QueryRunner,
   Table,
+  TableColumn,
   TableForeignKey
 } from 'typeorm'
 
@@ -16,45 +17,57 @@ export default class CreateVideos1616332030070 implements MigrationInterface {
   });
  */
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: 'videos',
-      columns: [
-        {
-          name: 'id',
-          type: 'varchar',
-          generationStrategy: 'uuid',
-          isPrimary: true
-        },
-        {
-          name: 'title',
-          type: 'varchar'
-        },
-        {
-          name: 'link',
-          type: 'varchar',
-          isUnique: true
-        },
-        {
-          name: 'description',
-          type: 'text'
-        },
-        {
+    await queryRunner.createTable(
+      new Table({
+        name: 'videos',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            generationStrategy: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'title',
+            type: 'varchar'
+          },
+          {
+            name: 'link',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'description',
+            type: 'text'
+          },
+          /*    {
           name: 'user_id',
           type: 'varchar',
           generationStrategy: 'uuid'
-        },
-        { name: 'created_at', type: 'timestamp', default: 'now()' },
-        { name: 'updated_at', type: 'timestamp', default: 'now()' }
-      ]
-    }))
-    await queryRunner.createForeignKey(
+        }, */
+
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+          { name: 'updated_at', type: 'timestamp', default: 'now()' }
+        ]
+      })
+    )
+
+    await queryRunner.addColumn(
       'videos',
+      new TableColumn({
+        name: 'user_id',
+        type: 'varchar',
+        generationStrategy: 'uuid'
+      })
+    )
+
+    await queryRunner.createForeignKey('videos',
       new TableForeignKey({
         name: 'VideoUser',
         columnNames: ['user_id'],
         referencedColumnNames: ['id'],
-        referencedTableName: 'users'
-        // onDelete: 'SET NULL'
+        referencedTableName: 'users',
+        onDelete: 'CASCADE'
         // onUpdate: 'SET NULL'
       })
     )
@@ -62,6 +75,7 @@ export default class CreateVideos1616332030070 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropForeignKey('videos', 'VideoUser')
+    await queryRunner.dropColumn('videos', 'user_id')
     await queryRunner.dropTable('videos')
   }
 }
