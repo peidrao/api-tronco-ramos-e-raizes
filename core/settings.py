3d7b971bd264265d9fa67032e04d222a87eac240
Pedro <contatopedrorn@gmail.com>
2021-07-29 20:02:24 -0300

    UPDATE: page_size = 9

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 9
 }
  
 SIMPLE_JWT = {
